cf9b34291a97ac21e46c79130e4b29d63a9b9c99
Oleksandra Pinchuk <ppinchukk.25@gmail.com>
2022-10-04 18:51:53 +0300

    done the notebook line

--- Test/ChatContentView.swift
+++ Test/ChatContentView.swift
@@ -8,17 +8,65 @@
 import SwiftUI
 
 struct ChatContentView: View {
-    
+   
+    @State private var message: String = ""
     let arrayOfMessages: [ChatContent]
     init(arrayOfMessages: [ChatContent]) {
         self.arrayOfMessages = arrayOfMessages
     }
     
     var body: some View {
-        List(arrayOfMessages) { message in
-            message
-        }
-        .flippedUpsideDown()
-        .listStyle(.plain)
+        VStack {
+            List(arrayOfMessages) { message in
+                message
+            }
+                .flippedUpsideDown()
+                .listStyle(.plain)
+            HStack {
+                Image("gallery")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.black)
+                    .opacity(0.6)
+                TextField("Write a message", text: $message)
+                    .frame(
+                        minWidth: 0,
+                        maxWidth: .infinity,
+                        minHeight: 0,
+                        maxHeight: 40
+                    )
+                    .padding(5)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(.gray, lineWidth: 1)
+                    )
+                Image("send")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.black)
+                    .opacity(0.6)
+                    
+            }
+        } //body
+        .padding(10)
+    }
+}
+
+struct ChatContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatContentView(arrayOfMessages: [
+          getChatContentView(),
+          getChatContentView(),
+          getChatContentView(),
+          getChatContentView(),
+          getChatContentView(),
+          getChatContentView(),
+          getChatContentView(),
+          getChatContentView(),
+          getChatContentView(),
+          getChatContentView()
+        ])
     }
 }
